0e73112cc4eefb3f23f5dfe6c34551be5b341255
nitin <nitinghimire.w@gmail.com>
2026-03-26 14:30:31 +0545

    Add simple boilerplate html and css for index.ejs

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ import pg from "pg";
 const port = 3000;
 const app = express();
 
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
 app.get("/", (req, res) => {
     res.render("index.ejs");
 })
